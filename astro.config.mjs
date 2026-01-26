f523e8be3c53ac201a0a60cbe83b1e198e2b5a65
patriciohorn <patriciohorn4@gmail.com>
2026-01-26 11:39:00 -0600

    Add Storyblok config and hero section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+//Setting up Storyblok
+import { storyblok } from "@storyblok/astro";
+import { loadEnv } from "vite";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        page: "storyblok/Page",
+        hero: "storyblok/Hero",
+      },
+    }),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
